33f77d9fb52ae5f1542dbfce016d039b8ebaa7b8
Samilly <samillynunes19@gmail.com>
2024-04-19 18:50:00 -0300

    UI: Tasks page structure and style added

--- src/pages/Tasks/index.js
+++ src/pages/Tasks/index.js
@@ -1,7 +1,37 @@
+import { useState } from 'react';
+import './tasks.css';
+
 function Tasks(){
+    const [taskInput, setTaskInput] = useState('');
+
+    function handleRegister(e){
+        e.preventDefault(); //p n atualizar a pagina
+    }
+
     return (
-        <div>
-            <h1>Tasks</h1>
+        <div className='tasks-container'>
+            <h1>Minhas tarefas</h1>
+
+            <form className='form' onSubmit={handleRegister} >
+                <textarea
+                    placeholder='Digite sua tarefa...'
+                    value={taskInput}
+                    onChange={(e)=> setTaskInput(e.target.value)}
+                />
+
+                <button type='submit' className='btn-register' >Salvar tarefa</button>
+            </form>
+
+            <article className='list'>
+                <p>Estudar javascript e reactJs</p>
+
+                <div>
+                    <button>Editar</button>
+                    <button className='btn-delete' >Concluir</button>
+                </div>
+            </article>
+
+            <button className='btn-logout' >Sair</button>
         </div>
     );
 }
